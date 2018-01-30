f5040ec71059d8982484f2bc04a4a796d9b6ca8e
locemarn <marcelon27@gmail.com>
2018-01-30 10:43:37 -0200

    funções de tratativas de error e debug no servidor

--- server.js
+++ server.js
@@ -19,8 +19,12 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route)
 
 server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
 console.log('Server ON: ' + port)
 
+// Funçoes tiradas do proprio express
+// normalizando a porta
 function normalizePort(val) {
   const port = parseInt(val, 10)
 
@@ -33,4 +37,38 @@ function normalizePort(val) {
   }
 
   return false
+}
+
+// Tratativa de erros
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string' ?
+    'Pipe ' + port : 
+    'Port ' + port
+    
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in user')
+      process.exit(1)
+      break;
+    default:
+      throw error
+  }
+}
+
+// pega as inf do servidr e starta o debug
+function onListening() {
+  const addr = server.address()
+  const bind = typeof addr === 'string' 
+    ? 'pipe ' + addr 
+    : 'port ' + addr.port
+
+  debug('Listening on ' + bind)
 }
