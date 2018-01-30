b1971a6ba4bcd44c90a4db6a95e68a8c47017926
locemarn <marcelon27@gmail.com>
2018-01-30 16:55:37 -0200

    conexao com mongodb / criação do model product

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express    = require('express')
 const bodyParser = require('body-parser')
+const mongoose   = require('mongoose')
 
 const app    = express()
 const router = express.Router()
+
+// conecta ao banco
+mongoose.connect('mongodb://locemarn:locemarn@ds046867.mlab.com:46867/nodestore')
 
 // carrega as rotas
 const indexRoute    = require('./routes/index-route')
